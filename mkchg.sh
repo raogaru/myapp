cf3c38889ba08c71ff26aa0111c91a3f20c5d169
raogaru <raogaruinc@gmail.com>
2021-04-25 16:59:57 -0400

    mkchg.sh

--- mkchg.sh
+++ mkchg.sh
@@ -1,11 +1,11 @@
 TEAM=$1
 CNT=$2
 cd $HOME/GitHub/myapp
+git checkout team-${TEAM}
 for i in 1..${CNT}
 do
-git checkout team-${TEAM}
 echo "$(date) change ${CNT} by ${TEAM} in file ${TEAM}/${TEAM}.txt" >> ${TEAM}/${TEAM}.txt
-done
 git add ${TEAM}/${TEAM}.txt
-git commit -m "RAO-${TEAM}:${TEAM}/${TEAM}.txt file added"
+git commit -m "RAO-${TEAM}:${TEAM}/${TEAM}.txt file modified cnt=${CNT}"
+done
 git push --set-upstream origin team-${TEAM}
