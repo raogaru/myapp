caf17dc6baba8ecc89bfd84f4b8ec2a1552f4065
raogaru <raogaruinc@gmail.com>
2021-04-25 17:06:37 -0400

    mkchg.sh

--- mkchg.sh
+++ mkchg.sh
@@ -8,4 +8,4 @@ echo "$(date) change ${CNT} by ${TEAM} in file ${TEAM}/${TEAM}.txt" >> ${TEAM}/$
 git add ${TEAM}/${TEAM}.txt
 git commit -m "RAO-${TEAM}:${TEAM}/${TEAM}.txt file modified cnt=${CNT}"
 done
-git push --set-upstream origin team-${TEAM}
+#git push --set-upstream origin team-${TEAM}
