72c30fc2359616b599ea0b41edce7d3f0e14b881
raogaru <raogaruinc@gmail.com>
2021-04-27 09:29:52 -0400

    test.sh

---
+++ test.sh
@@ -0,0 +1,18 @@
+echo "TEST $1 Started"
+
+s=$((1 + $RANDOM % 10))
+echo "Sleeping ... ${s} seconds"
+sleep ${s}
+
+r=$((1 + $RANDOM % 100))
+echo "TEST $1 Completed"
+
+echo "TEST result ${r}"
+
+if [ ${r} -le 5 ]; then
+	echo Test FAILED
+	exit 1
+else
+	echo Test SUCCESS
+	exit 0
+fi
