83048e08d5dec6c596075f473f563b4ec14b0260
raogaru <raogaruinc@gmail.com>
2021-04-27 19:58:07 -0400

    test.sh

--- test.sh
+++ test.sh
@@ -1,20 +1,18 @@
-echo "######################################################################"
-echo "TEST $1 Started"
+vLINE="######################################################################"
+vTEST="TEST{1}"
+echo "${vLINE}"
+echo "${vTEST} Started"
 
 s=$((1 + $RANDOM % 10))
 echo "Sleeping ... ${s} seconds"
 sleep ${s}
 
 r=$((1 + $RANDOM % 100))
-echo "TEST $1 Completed"
-echo "TEST result ${r}"
+echo "${vTEST} Completed"
+echo "${vTEST} Result ${r}"
 
 if [ ${r} -gt 5 ]; then
-	echo Test SUCCESS
-	echo "######################################################################"
-	exit 0
+	echo "${vTEST} SUCCESSS" ; echo "${vLINE}" ; exit 0
 else
-	echo Test FAILED
-	echo "######################################################################"
-	exit 1
+	echo "${vTEST} FAILED"; echo "${vLINE}" ; exit 1
 fi
