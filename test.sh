45d4a5a5f4e1ab07d3bc9f793d36bf69f50dc5fd
raogaru <raogaruinc@gmail.com>
2021-04-27 19:45:29 -0400

    test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
+echo "######################################################################"
 echo "TEST $1 Started"
 
 s=$((1 + $RANDOM % 10))
@@ -8,7 +9,7 @@ r=$((1 + $RANDOM % 100))
 echo "TEST $1 Completed"
 
 echo "TEST result ${r}"
-
+echo "######################################################################"
 if [ ${r} -le 5 ]; then
 	echo Test FAILED
 	exit 1
