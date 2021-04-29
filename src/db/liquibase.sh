024140e2ce166f67e7e66b558024c09b8af127ae
raogaru <raogaruinc@gmail.com>
2021-04-28 23:23:50 -0400

    liquibase.sh

--- src/db/liquibase.sh
+++ src/db/liquibase.sh
@@ -1,7 +1,7 @@
 V_DB=${1}
 liquibase \
   --driver=org.postgresql.Driver \
-  --classpath=postgresql-42.2.20.jar \
+  --classpath=/Users/rao/.jenkins/war/WEB-INF/lib/postgresql-42.2.20.jar \
   --url="jdbc:postgresql://localhost:5432/${V_DB}" \
   --changeLogFile=src/db/liquibase.xml \
   --username=rao \
