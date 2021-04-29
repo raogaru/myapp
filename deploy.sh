677b7b1def95cfe1f6d06fec8542d328860b4c42
raogaru <raogaruinc@gmail.com>
2021-04-28 23:00:30 -0400

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,16 @@ vTASK="DEPLOY:${vTARGET}"
 echo "${vLINE}"
 echo "${vTASK} Started"
 
+echo Deploy to ${vTARGET} database
+case " "${vTARGET}" in 
+"PIPE") src/db/liquibase.sh pipe ;;
+"SECURITY") src/db/liquibase.sh st ;;
+"PERFORMANCE") src/db/liquibase.sh pt ;;
+"ACCEPTANCE") src/db/liquibase.sh at ;;
+"INTERFACE") src/db/liquibase.sh it ;;
+"PRODUCTION") src/db/liquibase.sh prod ;;
+esac
+
 s=$((1 + $RANDOM % ${SLEEP_SECONDS}))
 echo "Sleeping ... ${s} seconds"
 sleep ${s}
