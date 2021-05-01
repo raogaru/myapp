3866df730da59566b7134a2f5e7f6d9c68be8212
raogaru <raogaruinc@gmail.com>
2021-05-01 00:10:58 -0400

    checkin.sh

--- checkin.sh
+++ checkin.sh
@@ -58,17 +58,16 @@ do
 	git checkout team-${TEAM}
 	git merge master -m "merge master to team-${TEAM}"
 
-for ((i = 1 ; i <= ${v_commits} ; i++))
-do
-	v_user=${TEAM}$((1 + $RANDOM % ${MAX_TEAM_MEMBERS}))
-	v_jira="JIRA-"$((1000 + $RANDOM))
-	v_file="src/app/dummy/file_"$((1 + $RANDOM % ${MAX_FILE_COUNT}))".txt"
+	for ((i = 1 ; i <= ${v_commits} ; i++))
+	do
+		v_user=${TEAM}$((1 + $RANDOM % ${MAX_TEAM_MEMBERS}))
+		v_jira="JIRA-"$((1000 + $RANDOM))
+		v_file="src/app/dummy/file_"$((1 + $RANDOM % ${MAX_FILE_COUNT}))".txt"
 
-	echo "$(date) change#${i} by user ${v_user} from team ${TEAM} in file ${v_file}" >> ${v_file}
-	git add ${v_file}
-	git commit -m "RAO-${v_jira}:${TEAM}${v_user}:${TEAM}/${TEAM}.txt file modification ${i}"
-done
-
+		echo "$(date) change#${i} by user ${v_user} from team ${TEAM} in file ${v_file}" >> ${v_file}
+		git add ${v_file}
+		git commit -m "RAO-${v_jira}:${TEAM}${v_user}:${TEAM}/${TEAM}.txt file modification ${i}"
+	done
 done
 }
 # ------------------------------------------------------------
@@ -81,15 +80,13 @@ do
 	LineHeader "List of commits by team \"${TEAM}\":"
 	ECHO "git log origin/master..build-${TEAM}" 
 	git log origin/master..origin/team-${TEAM} --pretty=format:"%ad:%h:%H:%an:%ae:%s" --date format:'%Y-%m-%d-%H-%M-%S' 
-	git log origin/master..origin/team-${TEAM} --pretty=format:"%ad:%h:%H:%an:%ae:%s" --date format:'%Y-%m-%d-%H-%M-%S'  > /tmp/${BUILD_NUM}/git_commits_by_${TEAM}.lst
 done
 }
 ######################################################################
 # MAIN PROGRAM
 ######################################################################
 f_checkin_code_by_team_members
-#f_flow_list_commits_by_each_team
-
+f_flow_list_commits_by_each_team
 
 LineHeader "Completed."
 
