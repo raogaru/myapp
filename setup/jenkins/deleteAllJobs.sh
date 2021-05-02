94c3c5c97517414a1cb596c600f72fb567472ddd
raogaru <raogaruinc@gmail.com>
2021-05-02 10:20:54 -0400

    deleteAllJobs.sh

--- setup/jenkins/deleteAllJobs.sh
+++ setup/jenkins/deleteAllJobs.sh
@@ -2,12 +2,6 @@
 # Delete All CI/CD Jenkins jobs
 # ######################################################################
 #
-./jenkins-cli.sh delete-job RAO-CD-10-Deploy-Security-Test
-./jenkins-cli.sh delete-job RAO-CD-20-Deploy-Performance-Test
-./jenkins-cli.sh delete-job RAO-CD-30-Deploy-Acceptance-Test
-./jenkins-cli.sh delete-job RAO-CD-40-Deploy-Interface-Test
-./jenkins-cli.sh delete-job RAO-CD-50-Deploy-Production
-#
 ./jenkins-cli.sh delete-job RAO-CI-00-Pipeline
 ./jenkins-cli.sh delete-job RAO-CI-10-Enter-Build
 ./jenkins-cli.sh delete-job RAO-CI-20-Team-Build
@@ -16,7 +10,14 @@
 ./jenkins-cli.sh delete-job RAO-CI-50-Deploy-Pipeline-Test
 ./jenkins-cli.sh delete-job RAO-CI-55-Execute-Pipeline-Test
 #
-./jenkins-cli.sh delete-view RAO-CD
+./jenkins-cli.sh delete-job RAO-CD-00-Pipeline
+./jenkins-cli.sh delete-job RAO-CD-10-Deploy-Security-Test
+./jenkins-cli.sh delete-job RAO-CD-20-Deploy-Performance-Test
+./jenkins-cli.sh delete-job RAO-CD-30-Deploy-Acceptance-Test
+./jenkins-cli.sh delete-job RAO-CD-40-Deploy-Interface-Test
+./jenkins-cli.sh delete-job RAO-CD-50-Deploy-Production
+#
 ./jenkins-cli.sh delete-view RAO-CI
+./jenkins-cli.sh delete-view RAO-CD
 #
 
