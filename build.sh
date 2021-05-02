e431014f541a05dabe06bd2ba186c817e1914d01
raogaru <raogaruinc@gmail.com>
2021-05-02 19:02:50 -0400

    build.sh

--- build.sh
+++ build.sh
@@ -4,10 +4,20 @@ vTYPE="${1}"
 vTEAM="${2}"
 vTASK="BUILD:${vTYPE}-${vTEAM}"
 ECHO "${vLINE}"
-ECHO "${vTASK} Started"
 
 ECHO "Contents of ${BUILD_ENV_FILE}"
-cat ${BUILD_ENV_FILE}
+cat ${BUILD_ENV_FILE} 
+
+if [ "${vTYPE}" == "team" ]; then
+	x=$(cat ${BUILD_ENV_FILE}|grep TEAM_COMMITS_${vTEAM}|cut -f2 -d"=")
+  	if [ "${x}" != "YES" ]; then
+		ECHO "No commits. No build needed"
+		ADDENV "TEAM_BUILD_${vTEAM}=N/A"
+		exit 0
+	fi
+fi
+
+ECHO "${vTASK} Started"
 
 s=$((1 + $RANDOM % ${RC_SLEEP_SECONDS}))
 echo "Sleeping ... ${s} seconds"
@@ -18,7 +28,14 @@ ECHO "${vTASK} Completed"
 ECHO "${vTASK} Result ${r}"
 
 if [ ${r} -gt ${RC_PASS_SCORE} ]; then
-	ECHO "${vTASK} SUCCESSS" ; echo "${vLINE}" ; exit 0
+	ECHO "${vTASK} SUCCESSS"
+	ADDENV "TEAM_BUILD_${vTEAM}=SUCCESSS"
+	echo "${vLINE}" 
+	exit 0
 else
-	ECHO "${vTASK} FAILED"; echo "${vLINE}" ; exit 1
+	ECHO "${vTASK} FAILED"
+	ADDENV "TEAM_BUILD_${vTEAM}=FAILED"
+	echo "${vLINE}" 
+	exit 1
 fi
+# ######################################################################
