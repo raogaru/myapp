79b489d0bcfdba875d34d6a18e877f5f9e7cbac7
raogaru <raogaruinc@gmail.com>
2021-05-02 22:26:02 -0400

    setup/jenkins/deleteAllJobs.sh

--- setup/jenkins/deleteAllJobs.sh
+++ setup/jenkins/deleteAllJobs.sh
@@ -2,18 +2,31 @@
 # Delete All CI/CD Jenkins jobs
 # ######################################################################
 #
-./jenkins-cli.sh delete-job RAO-CI-00-Pipeline
 ./jenkins-cli.sh delete-job RAO-CI-10-Enter-Build
-./jenkins-cli.sh delete-job RAO-CI-20-Team-Build
-./jenkins-cli.sh delete-job RAO-CI-20-Team-Build-MARS
-./jenkins-cli.sh delete-job RAO-CI-20-Team-Build-VENUS
-./jenkins-cli.sh delete-job RAO-CI-20-Team-Build-PLUTO
-./jenkins-cli.sh delete-job RAO-CI-30-System-Build
-./jenkins-cli.sh delete-job RAO-CI-40-Release-Build
-./jenkins-cli.sh delete-job RAO-CI-50-Deploy-Pipeline-Test
-./jenkins-cli.sh delete-job RAO-CI-55-Execute-Pipeline-Test
 #
-./jenkins-cli.sh delete-job RAO-CD-00-Pipeline
+./jenkins-cli.sh delete-job RAO-CI-20-Build-Team-MARS
+./jenkins-cli.sh delete-job RAO-CI-20-Build-Team-VENUS
+./jenkins-cli.sh delete-job RAO-CI-20-Build-Team-PLUTO
+#
+./jenkins-cli.sh delete-job RAO-CI-30-Deploy-Team-MARS
+./jenkins-cli.sh delete-job RAO-CI-30-Deploy-Team-VENUS
+./jenkins-cli.sh delete-job RAO-CI-30-Deploy-Team-PLUTO
+#
+./jenkins-cli.sh delete-job RAO-CI-40-Test-Team-MARS
+./jenkins-cli.sh delete-job RAO-CI-40-Test-Team-VENUS
+./jenkins-cli.sh delete-job RAO-CI-40-Test-Team-PLUTO
+#
+./jenkins-cli.sh delete-job RAO-CI-50-System-Gate
+./jenkins-cli.sh delete-job RAO-CI-51-Build-System
+./jenkins-cli.sh delete-job RAO-CI-52-Deploy-System
+./jenkins-cli.sh delete-job RAO-CI-53-Test-System
+#
+./jenkins-cli.sh delete-job RAO-CI-60-Release-Gate
+./jenkins-cli.sh delete-job RAO-CI-61-Release-Prepare
+./jenkins-cli.sh delete-job RAO-CI-62-Release-Verify
+./jenkins-cli.sh delete-job RAO-CI-63-Release-Publish
+./jenkins-cli.sh delete-job RAO-CI-64-Release-Notify
+#
 ./jenkins-cli.sh delete-job RAO-CD-10-Deploy-Security-Test
 ./jenkins-cli.sh delete-job RAO-CD-20-Deploy-Performance-Test
 ./jenkins-cli.sh delete-job RAO-CD-30-Deploy-Acceptance-Test
@@ -23,4 +36,4 @@
 ./jenkins-cli.sh delete-view RAO-CI
 ./jenkins-cli.sh delete-view RAO-CD
 #
-
+# ######################################################################
