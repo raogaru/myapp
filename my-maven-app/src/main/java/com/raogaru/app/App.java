3718d5ed8e43027309673f469483483c840e7ef1
raogaru <raogaruinc@gmail.com>
2021-05-17 22:16:54 -0400

    my-maven-app/

---
+++ my-maven-app/src/main/java/com/raogaru/app/App.java
@@ -0,0 +1,12 @@
+package com.raogaru.app;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("A NullPointerException is a drag...");
+        Object myObject = null;
+        System.out.println("... but thankfully, Checker has our back: http://checkerframework.org");
+    }
+}
